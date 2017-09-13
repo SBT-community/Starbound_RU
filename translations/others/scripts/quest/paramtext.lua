86bfd3ee0130e8a50bbbfd482eea505cdb2cff57
xomachine <xomachiner@gmail.com>
2017-09-14 01:34:25 +0300

    <Smbdy.objective> для дательного падежа имени.

--- translations/others/scripts/quest/paramtext.lua
+++ translations/others/scripts/quest/paramtext.lua
@@ -10,7 +10,29 @@ local function getCountEnding(count)
   else return "" end
 end
 
-local function convertNounAndAdjective(phrase)
+local function convertNameToObjective(name, gender)
+  if gender == nil then return name end
+  local consonants = {"ц", "к", "н", "ш", "щ", "з", "х", "ф", "в", "п", "р",
+                      "л", "д", "ж", "ч", "с", "м", "т", "б"}
+  local variants = {
+    [1] = {match = {"й"}, sub = {male = "ю", female = "%1"},},
+    [2] = {match = {"ия"}, sub = {male = "ие", female = "ии"},},
+    [3] = {match = {"ь"}, sub = {male = "ю", female = "и"},},
+    [4] = {match = {"а", "я"}, sub = {male = "е", female = "е"},},
+    [5] = {match = consonants, sub = {male = "%1у", female = "%1"},},
+  }
+  local result, count = name, 0
+  for i = 1, #variants, 1 do
+    for _, pat in pairs(variants[i].match) do
+      result, count = name:gsub("("..pat..")$", variants[i].sub[gender])
+      if count > 0 then return result end
+    end
+  end
+  return name
+end
+
+local function convertNounAndAdjective(phrase, gender)
+  if gender ~= nil then return phrase end
   local result = ""
   local isFemine = true
   for word in phrase:gmatch("%S+") do
@@ -38,7 +60,9 @@ end
 
 function questParameterText(paramValue, caseModifier)
   caseModifier = caseModifier or function(a) return a end
-  if paramValue.name then return paramValue.name end
+  if paramValue.name then
+    return caseModifier(paramValue.name, paramValue.gender)
+  end
 
   if paramValue.type == "item" then
     return caseModifier(itemShortDescription(paramValue.item))
@@ -72,6 +96,7 @@ function questParameterTags(parameters)
   for k, v in pairs(parameters) do
     result[k] = questParameterText(v)
     result[k..".reflexive"] = questParameterText(v, convertNounAndAdjective)
+    result[k..".objective"] = questParameterText(v, convertNameToObjective)
   end
   return result
 end
