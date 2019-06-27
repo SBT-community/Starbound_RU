026a764fce326e05e936cd10b65ae5fca1942ca7
xomachine <xomachiner@gmail.com>
2019-06-27 20:42:40 +0300

    Translation in prebounty script

--- translations/others/quests/bounty/pre_bounty.lua
+++ translations/others/quests/bounty/pre_bounty.lua
@@ -18,7 +18,7 @@ end
 
 function setText()
   local tags = util.generateTextTags(quest.parameters().text.tags)
-  quest.setTitle(sb.replaceTags("^orange;Bounty: ^green;<bounty.name>", tags))
+  quest.setTitle(sb.replaceTags("^orange;Цель: ^green;<bounty.name>", tags))
 
   local textCons
   for i, q in pairs(quest.questArcDescriptor().quests) do
